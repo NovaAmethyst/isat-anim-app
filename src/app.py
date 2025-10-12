acd4bed5aedc4691c33c8a77db1ca747f7546961
Juliette Jacquot <juliette.jacquot@epita.fr>
2025-10-12 19:23:09 +0200

    feat: action list

--- src/app.py
+++ src/app.py
@@ -17,6 +17,7 @@ class AnimationEditorApp(tk.Tk):
         self.actors: list[Actor] = []
         self.actors_save_status: list[tuple[bool, str]] = []
         self.curr_actor_idx: Optional[int] = None
+        self.curr_action_idx: Optional[int] = None
 
         self.create_tabs()
     
@@ -52,7 +53,7 @@ class AnimationEditorApp(tk.Tk):
 
         # Right: Actor details, actions and action components
         actor_detail_frame = tk.Frame(self.actor_frame)
-        actor_detail_frame.pack(side="right", fill="both", expand=True)
+        actor_detail_frame.pack(side="right", fill="both", expand=True, padx=10, pady=10)
 
         # Top right: name display and save buttons
         actor_name_frame = tk.Frame(actor_detail_frame)
@@ -61,26 +62,61 @@ class AnimationEditorApp(tk.Tk):
         self.actor_name_label.pack(side="left", fill="x")
         tk.Button(actor_name_frame, text="Save As", command=self.save_actor_as).pack(side="right", padx=2)
         tk.Button(actor_name_frame, text="Save", command=self.save_actor).pack(side="right", padx=2)
-    
+
+        # Middle right: action list
+        def display_action(action: Action) -> str:
+            return action.name
+
+        self.action_listbox = EditableListFrame(
+            parent=actor_detail_frame,
+            display_func=display_action,
+            buttons_info=[
+                {"text": "Add Action", "command": self.add_action},
+                {"text": "Delete Action", "command": self.delete_action},
+            ],
+            btns_per_row=3,
+            select_func=self.select_action,
+            double_func=self.rename_action,
+        )
+        self.action_listbox.pack(side="top", fill="both", expand=True)
+
+    def set_actor_all(self):
+        actor_idx = self.curr_actor_idx
+        action_idx = self.curr_action_idx
+
+        self.clean_actor()
+        self.actor_listbox.clean()
+
+        self.actor_listbox.set(self.actors)
+        if action_idx is not None:
+            self.set_action(action_idx)
+        elif actor_idx is not None:
+            self.set_actor(actor_idx)
+
     def unsaved_actor(self, idx: int):
         self.actors_save_status[idx] = (False, self.actors_save_status[idx][1])
 
         self.actor_listbox.clean()
         self.actor_listbox.set(self.actors)
-    
+
     def clean_actor(self):
+        self.clean_action()
+
         self.actor_listbox.select_clear()
         self.curr_actor_idx = None
 
         self.actor_name_label["text"] = ""
-    
+
     def set_actor(self, idx: int):
         self.clean_actor()
 
         self.curr_actor_idx = idx
         self.actor_listbox.select(idx)
 
-        self.actor_name_label["text"] = self.actors[idx].name
+        actor = self.actors[idx]
+
+        self.actor_name_label["text"] = actor.name
+        self.action_listbox.set(actor.actions)
     
     def _add_or_replace_actor(self, actor: Actor, path: str | None = None, idx: int | None = None):
         save_status = (path is not None, path)
@@ -91,8 +127,8 @@ class AnimationEditorApp(tk.Tk):
         else:
             self.actors[idx] = actor
             self.actors_save_status[idx] = save_status
-        
-        self.actor_listbox.set(self.actors)
+
+        self.set_actor_all()
         self.set_actor(idx)
     
     def add_actor(self):
@@ -134,7 +170,7 @@ class AnimationEditorApp(tk.Tk):
         self.actors.pop(idx)
         self.actors_save_status.pop(idx)
         self.clean_actor()
-        self.actor_listbox.set(self.actors)
+        self.set_actor_all()
 
     def select_actor(self, event, listbox):
         idx = listbox.curselection()
@@ -153,6 +189,73 @@ class AnimationEditorApp(tk.Tk):
         actor_idx = self.curr_actor_idx
         old_name = self.actors[actor_idx].name
         create_renaming_entry(listbox, actor_idx, old_name, save_actor_name)
+
+    def clean_action(self):
+        self.action_listbox.select_clear()
+        self.curr_action_idx = None
+
+    def set_action(self, idx):
+        if self.curr_actor_idx is None:
+            return
+        actor_idx = self.curr_actor_idx
+
+        self.clean_actor()
+        self.set_actor(actor_idx)
+        self.curr_action_idx = idx
+        self.action_listbox.select(idx)
+
+    def add_action(self):
+        if self.curr_actor_idx is None:
+            return
+        actor_idx = self.curr_actor_idx
+        actor = self.actors[actor_idx]
+
+        action_names = [action.name for action in actor.actions]
+        i = 1
+        while f"Action {i}" in action_names:
+            i += 1
+
+        action = Action(name=f"Action {i}")
+        actor.actions.append(action)
+        idx = len(actor.actions) - 1
+
+        self.unsaved_actor(actor_idx)
+        self.set_action(idx)
+
+    def delete_action(self):
+        if self.curr_actor_idx is None or self.curr_action_idx is None:
+            return
+
+        actor_idx, action_idx = self.curr_actor_idx, self.curr_action_idx
+        actor = self.actors[actor_idx]
+        actor.actions.pop(action_idx)
+
+        self.unsaved_actor(actor_idx)
+        self.set_actor(actor_idx)
+
+    def select_action(self, event, listbox):
+        if self.curr_actor_idx is None:
+            return
+
+        idx = listbox.curselection()
+        if idx and idx[0] != self.curr_action_idx:
+            self.set_action(idx[0])
+
+    def rename_action(self, event, listbox):
+        if self.curr_actor_idx is None or self.curr_action_idx is None:
+            return
+
+        actor_idx = self.curr_actor_idx
+        actor = self.actors[actor_idx]
+
+        def save_action_name(idx: int, name: str):
+            actor.actions[idx].name = name
+            self.unsaved_actor(actor_idx)
+            self.set_action(idx)
+
+        action_idx = self.curr_action_idx
+        old_name = actor.actions[action_idx].name
+        create_renaming_entry(listbox, actor_idx, old_name, save_action_name)
 
     def save_actor_as(self):
         if self.curr_actor_idx is None:
@@ -190,7 +293,7 @@ class AnimationEditorApp(tk.Tk):
             json.dump(actor_dict, f, indent=2)
 
         self.actors_save_status[idx] = (True, path)
-        self.actor_listbox.set(self.actors)
+        self.set_actor_all()
         self.set_actor(idx)
 
     def window_close(self):
